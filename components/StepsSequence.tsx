5776f4f40a696d6a4e8fc7242524ec6b382c29ea
shepherd-moves <nelsonjordy2@gmail.com>
2023-11-30 22:00:23 +1100

    fixed error on StepsSequence

--- components/StepsSequence.tsx
+++ components/StepsSequence.tsx
@@ -2,17 +2,16 @@ import React from "react";
 import { Step, StepsSequenceProps } from "../types";
 
 function StepsSequence({ steps }: StepsSequenceProps) {
-    return (
-        <div className="steps-sequence">
-          {steps.map((step, index) => (
-            <div key={index} className="step">
-              <h3>{step.title}</h3>
-              <p>{step.description}</p>
-            </div>
-          ))}
+  return (
+    <div className="steps-sequence">
+      {steps.map((step, index) => (
+        <div key={index} className="step">
+          <h3>{step.title}</h3>
+          <p>{step.description}</p>
         </div>
-      );
-    };
+      ))}
+    </div>
+  );
 }
 
 export default StepsSequence;
