f34c2b3c925e7c6f389a4dbe1fc04abc2592225e
shepherd-moves <nelsonjordy2@gmail.com>
2023-11-30 21:55:55 +1100

    Added message above Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -41,77 +41,91 @@ const itemVariants = {
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
+  const [showBanner, setShowBanner] = useState(true);
 
   return (
-    <nav>
-      <div className="flex justify-between items-center h-24 max-w-xl md:max-w-5xl mx-auto px-4">
-        <Link href={"/"}>
-          <img
-            src="/ts-plug.svg"
-            className="h-[60px] w-[60px] object-contain"
-            alt="Tuckshop"
-          />
-        </Link>
-        <ul className=" hidden md:flex gap-2 text-l font-semibold text-inter capitalize">
-          <li>
-            <Link href="/">Home</Link>
-          </li>
-          <li>
-            <Link href="/collections">Collections</Link>
-          </li>
-          <li>
-            <Link href="/cart">Cart</Link>
-          </li>
-        </ul>
-        <button className="md:hidden">
-          {isOpen ? (
-            <AiOutlineClose onClick={() => setIsOpen(!isOpen)} />
-          ) : (
-            <FaBars onClick={() => setIsOpen(!isOpen)} />
-          )}
-        </button>
-      </div>
-      <div className="flex md:hidden">
-        <AnimatePresence>
-          {isOpen && (
-            <motion.aside
-              initial={{ width: 0 }}
-              animate={{
-                width: 300,
-              }}
-              exit={{
-                width: 0,
-                transition: { delay: 0.7, duration: 0.3 },
-              }}
-              className="bg-white w-[auto] h-screen "
-            >
-              <motion.div
-                className="container mt-[4.5rem] mb-[4.5rem] ml-[1.4rem] mr-[1.4rem] "
-                initial="closed"
-                animate="open"
-                exit="closed"
-                variants={sideVariants}
+    <>
+      {showBanner && (
+        //div with p tag and button to close it. Center the text and flex the button to the right
+        <div className="bg-black text-white text-center py-2 px-4 flex justify-center items-center">
+          <p className="flex text-sm md:text-base">
+            <strong>
+              If you want to purchase an item, pelase DM us on IG! - @tucks_shop
+            </strong>
+          </p>
+        </div>
+      )}
+
+      <nav>
+        <div className="flex justify-between items-center h-24 max-w-xl md:max-w-5xl mx-auto px-4">
+          <Link href={"/"}>
+            <img
+              src="/ts-plug.svg"
+              className="h-[60px] w-[60px] object-contain"
+              alt="Tuckshop"
+            />
+          </Link>
+          <ul className=" hidden md:flex gap-2 text-l font-semibold text-inter capitalize">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/collections">Collections</Link>
+            </li>
+            <li>
+              <Link href="/cart">Cart</Link>
+            </li>
+          </ul>
+          <button className="md:hidden">
+            {isOpen ? (
+              <AiOutlineClose onClick={() => setIsOpen(!isOpen)} />
+            ) : (
+              <FaBars onClick={() => setIsOpen(!isOpen)} />
+            )}
+          </button>
+        </div>
+        <div className="flex md:hidden">
+          <AnimatePresence>
+            {isOpen && (
+              <motion.aside
+                initial={{ width: 0 }}
+                animate={{
+                  width: 300,
+                }}
+                exit={{
+                  width: 0,
+                  transition: { delay: 0.7, duration: 0.3 },
+                }}
+                className="bg-white w-[auto] h-screen "
               >
-                {links.map((link) => (
-                  <div className="flex items-center">
-                    <link.logo className="text-4xl  text-black" />
-                    <motion.a
-                      key={link.id}
-                      href={link.to}
-                      whileHover={{ scale: 1.1 }}
-                      variants={itemVariants}
-                      className="text-black decoration-none text-[1.75rem] font-bold block m-[20px]"
-                    >
-                      {link.name}
-                    </motion.a>
-                  </div>
-                ))}
-              </motion.div>
-            </motion.aside>
-          )}
-        </AnimatePresence>
-      </div>
-    </nav>
+                <motion.div
+                  className="container mt-[4.5rem] mb-[4.5rem] ml-[1.4rem] mr-[1.4rem] "
+                  initial="closed"
+                  animate="open"
+                  exit="closed"
+                  variants={sideVariants}
+                >
+                  {links.map((link) => (
+                    <div className="flex items-center">
+                      <link.logo className="text-4xl  text-black" />
+                      <motion.a
+                        key={link.id}
+                        href={link.to}
+                        whileHover={{ scale: 1.1 }}
+                        variants={itemVariants}
+                        className="text-black decoration-none text-[1.75rem] font-bold block m-[20px]"
+                      >
+                        {link.name}
+                      </motion.a>
+                    </div>
+                  ))}
+                </motion.div>
+              </motion.aside>
+            )}
+          </AnimatePresence>
+        </div>
+      </nav>
+    </>
   );
 }
 
